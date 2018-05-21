a85c43ddd02826bdd6389c991d69a6b71adf3873
AlbertRui <isruizhang@gmail.com>
2018-05-21 23:48:11 +0800

    initial

---
+++ src/page/index/index.js
@@ -0,0 +1,8 @@
+/*
+* @Author: Administrator
+* @Date:   2018-05-21 16:47:19
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-05-21 17:46:07
+*/
+'use strict';
+console.log('hello index');
---
+++ src/page/login/index.js
@@ -0,0 +1,8 @@
+/*
+* @Author: Administrator
+* @Date:   2018-05-21 17:42:58
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-05-21 17:44:20
+*/
+'use strict';
+console.log('hello login');
