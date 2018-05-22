6cefea04c1c996f34bbce0970e3da76ec4646d54
AlbertRui <isruizhang@gmail.com>
2018-05-22 23:52:18 +0800

    add request for util

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,7 +2,9 @@
 * @Author: Administrator
 * @Date:   2018-05-21 16:47:19
 * @Last Modified by:   Administrator
-* @Last Modified time: 2018-05-21 17:46:07
+* @Last Modified time: 2018-05-22 22:57:06
 */
 'use strict';
+var $ = require('jquery');
+$('body').html('hello word');
 console.log('hello index');
---
+++ src/util/ubaby.js
@@ -0,0 +1,16 @@
+/*
+* @Author: Administrator
+* @Date:   2018-05-22 22:56:44
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-05-22 23:06:02
+*/
+'use strict';
+var _ubaby = {
+    request : function (param) {
+        $.ajax({
+            type : param, method || 'get',
+            url : 
+        });
+    }
+};
+module.exports = _ubaby;
