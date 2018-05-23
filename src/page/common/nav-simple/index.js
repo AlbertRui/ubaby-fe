0e9c231396a43e67d8ccd7d15ca33c772b16e38a
AlbertRui <isruizhang@gmail.com>
2018-05-23 23:49:08 +0800

    add logo for index.html

---
+++ src/page/common/nav-simple/index.js
@@ -0,0 +1,8 @@
+/*
+* @Author: Administrator
+* @Date:   2018-05-23 23:36:32
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-05-23 23:48:28
+*/
+'use strict';
+require('./index.css');
--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,7 +2,8 @@
 * @Author: Administrator
 * @Date:   2018-05-21 16:47:19
 * @Last Modified by:   Administrator
-* @Last Modified time: 2018-05-23 17:24:33
+* @Last Modified time: 2018-05-23 23:40:02
 */
 'use strict';
+require('page/common/nav-simple/index.js');
 var _ubaby = require('util/ubaby.js');
