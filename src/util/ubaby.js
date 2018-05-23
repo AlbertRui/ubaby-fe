7858b3997a09af916ff50b68dc17cef05a433a4b
AlbertRui <isruizhang@gmail.com>
2018-05-23 17:19:10 +0800

    add methods for ubaby.js

--- src/util/ubaby.js
+++ src/util/ubaby.js
@@ -1,16 +1,85 @@
 /*
-* @Author: Administrator
-* @Date:   2018-05-22 22:56:44
-* @Last Modified by:   Administrator
-* @Last Modified time: 2018-05-22 23:06:02
-*/
+ * @Author: Administrator
+ * @Date:   2018-05-22 22:56:44
+ * @Last Modified by:   Administrator
+ * @Last Modified time: 2018-05-23 17:18:35
+ */
 'use strict';
+var Hogan = require('hogan');
+var conf = {
+    serverHost: '';
+};
 var _ubaby = {
-    request : function (param) {
+    //网络请求
+    request: function(param) {
+        var _this = this;
         $.ajax({
-            type : param, method || 'get',
-            url : 
+            type: param.method || 'get',
+            url: param.url || '',
+            dataType: param.type || 'json',
+            data: param.data || '',
+            success: function(res) {
+                //请求成功
+                if (0 === res.status) {
+                    typeof param.success === 'function' && param.success(res.data, res.msg);
+                } else if (10 === res.status) {
+                    //需要强制登录
+                    _this.doLogin();
+                } else if (1 === res.status) {
+                    typeof param.error === 'function' && param.error(res.msg);
+                }
+            },
+            error: function(err) {
+                typeof param.error === 'function' && param.error(err.statusText);
+            }
         });
+    },
+    //获取服务器url
+    getServerUrl: function(path) {
+        return conf.serverHost + path;
+    },
+    //获取url参数
+    getUrlParam: function(name) {
+        var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)');
+        var result = window.location.search.substr(1).match(reg);
+        return result ? decodeURIComponent(result[2]) : null;
+    },
+    //渲染html模板
+    renderHtml: function(htmlTemplate, data) {
+        var template = Hagan.compile(htmlTemplate),
+            result = template.render(data);
+        return result;
+    },
+    //成功提示
+    successTips: function(msg) {
+        alert(msg || '操作成功！');
+    },
+    //错误提示
+    errorTips: function(msg) {
+        alert(msg || '哪里不对了');
+    },
+    //字段的验证,支持是否为空，支持非空判断，手机邮箱
+    validate: function(value, type) {
+        var value = $.trim(value);
+        //非空验证
+        if ('require' === type) {
+            return !!value;
+        }
+        //手机号验证
+        if ('phone' === type) {
+            return /^1\d{10}$/.test(value);
+        }
+        if ('email' === type) {
+            return /^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/.test(value);
+        }
+    },
+    //统一登录处理
+    doLogin: function() {
+        window.location.href = './login.html?redirect=' + encodeURIComponent(window.location.href);
+    },
+    //返回主页
+    goHome: function() {
+        window.location.href = './index.html';
     }
 };
 module.exports = _ubaby;
