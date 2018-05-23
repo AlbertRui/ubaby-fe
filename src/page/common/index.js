16c70a86e5635b10c99fcf265f01ed199ec578b4
AlbertRui <isruizhang@gmail.com>
2018-05-23 18:37:19 +0800

    add styles by layout.css

--- src/page/common/index.js
+++ src/page/common/index.js
@@ -2,7 +2,8 @@
 * @Author: Administrator
 * @Date:   2018-05-23 17:20:51
 * @Last Modified by:   Administrator
-* @Last Modified time: 2018-05-23 17:24:36
+* @Last Modified time: 2018-05-23 18:35:23
 */
 'use strict';
 require('./layout.css');
+require('node_modules/font-awesome/css/font-awesome.min.css');
