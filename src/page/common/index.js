eee8094f023b30fb24bb336faa1f5739e35f12e0
AlbertRui <isruizhang@gmail.com>
2018-05-23 17:31:15 +0800

    add head-common for view/layout

---
+++ src/page/common/index.js
@@ -0,0 +1,8 @@
+/*
+* @Author: Administrator
+* @Date:   2018-05-23 17:20:51
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-05-23 17:24:36
+*/
+'use strict';
+require('./layout.css');
--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,9 +2,7 @@
 * @Author: Administrator
 * @Date:   2018-05-21 16:47:19
 * @Last Modified by:   Administrator
-* @Last Modified time: 2018-05-22 22:57:06
+* @Last Modified time: 2018-05-23 17:24:33
 */
 'use strict';
-var $ = require('jquery');
-$('body').html('hello word');
-console.log('hello index');
+var _ubaby = require('util/ubaby.js');
