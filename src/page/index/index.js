871071db9077215d25d2d283c5254c6ff6133ad7
AlbertRui <isruizhang@gmail.com>
2018-05-25 21:46:14 +0800

    completed header for ubaby

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,8 +2,9 @@
 * @Author: Administrator
 * @Date:   2018-05-21 16:47:19
 * @Last Modified by:   Administrator
-* @Last Modified time: 2018-05-25 20:05:04
+* @Last Modified time: 2018-05-25 21:40:47
 */
 'use strict';
+require('page/common/header/index.js');
 require('page/common/nav/index.js');
 var _ubaby = require('util/ubaby.js');
