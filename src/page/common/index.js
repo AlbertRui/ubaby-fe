3f6eb7ff3f7c1353b5026c97cde2cdb9039f9db4
AlbertRui <isruizhang@gmail.com>
2018-05-25 21:02:25 +0800

    add footer for ubaby

--- src/page/common/index.js
+++ src/page/common/index.js
@@ -2,8 +2,9 @@
 * @Author: Administrator
 * @Date:   2018-05-23 17:20:51
 * @Last Modified by:   Administrator
-* @Last Modified time: 2018-05-23 18:35:23
+* @Last Modified time: 2018-05-25 20:54:54
 */
 'use strict';
 require('./layout.css');
 require('node_modules/font-awesome/css/font-awesome.min.css');
+require('./footer/index.css');
