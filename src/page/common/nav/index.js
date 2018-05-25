6f85f5c02f06d6ebdd4813ff3185ca57f1977e05
AlbertRui <isruizhang@gmail.com>
2018-05-25 20:49:00 +0800

    update nav.html for ubaby

---
+++ src/page/common/nav/index.js
@@ -0,0 +1,7 @@
+/*
+* @Author: Administrator
+* @Date:   2018-05-25 20:03:15
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-05-25 20:04:32
+*/
+require('./index.css');
--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,8 +2,8 @@
 * @Author: Administrator
 * @Date:   2018-05-21 16:47:19
 * @Last Modified by:   Administrator
-* @Last Modified time: 2018-05-23 23:40:02
+* @Last Modified time: 2018-05-25 20:05:04
 */
 'use strict';
-require('page/common/nav-simple/index.js');
+require('page/common/nav/index.js');
 var _ubaby = require('util/ubaby.js');
