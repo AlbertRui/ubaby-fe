b5921d96885e9ba842e8fde1e5ed0a518931fd55
AlbertRui <isruizhang@gmail.com>
2018-05-27 11:29:29 +0800

    add index module for page

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,15 +1,29 @@
 /*
-* @Author: Administrator
-* @Date:   2018-05-21 16:47:19
-* @Last Modified by:   Administrator
-* @Last Modified time: 2018-05-25 23:54:59
-*/
+ * @Author: Administrator
+ * @Date:   2018-05-21 16:47:19
+ * @Last Modified by:   Administrator
+ * @Last Modified time: 2018-05-27 11:28:20
+ */
 'use strict';
+require('./index.css');
 require('page/common/nav/index.js');
+require('util/slider/index.js');
 require('page/common/header/index.js');
 var navSide = require('page/common/nav-side/index.js');
+var templateBanner = require('./banner.string');
 var _ubaby = require('util/ubaby.js');
 
-navSide.init({
-    name : 'user-center'
+$(function() {
+    // 渲染banner的html
+    var bannerHtml = _ubaby.renderHtml(templateBanner);
+    $('.banner-con').html(bannerHtml);
+    // 初始化banner
+    var $slider = $('.banner').unslider({
+        dots: true
+    });
+    // 前一张和后一张操作的事件绑定
+    $('.banner-con .banner-arrow').click(function() {
+        var forward = $(this).hasClass('prev') ? 'prev' : 'next';
+        $slider.data('unslider')[forward]();
+    });
 });
---
+++ src/util/slider/index.js
@@ -0,0 +1,9 @@
+/*
+* @Author: Administrator
+* @Date:   2018-05-27 10:59:14
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-05-27 11:01:40
+*/
+'use strict';
+require('./index.css');
+require('./unslider.js');
