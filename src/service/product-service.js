d6a7244448f5a7a0f8faf36835027fc644c7d23d
AlbertRui <isruizhang@gmail.com>
2018-05-27 22:28:11 +0800

    add detail for page

--- src/service/product-service.js
+++ src/service/product-service.js
@@ -2,7 +2,7 @@
  * @Author: Administrator
  * @Date:   2018-05-27 15:56:56
  * @Last Modified by:   Administrator
- * @Last Modified time: 2018-05-27 17:06:29
+ * @Last Modified time: 2018-05-27 22:06:45
  */
 'use strict';
 
@@ -14,6 +14,17 @@ var _product = {
         _ubaby.request({
             url: _ubaby.getServerUrl('/product/list.do'),
             data: listParam,
+            success: resolve,
+            error: reject
+        });
+    },
+    // 获取商品详细信息
+    getProductDetail: function(productId, resolve, reject) {
+        _ubaby.request({
+            url: _ubaby.getServerUrl('/product/detail.do'),
+            data: {
+                productId: productId
+            },
             success: resolve,
             error: reject
         });
