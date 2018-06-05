79bf8695872abdc5b9d09b290f784eab12fa5d57
AlbertRui <isruizhang@gmail.com>
2018-06-05 21:18:57 +0800

    add about for page

---
+++ src/page/about/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Administrator
+ * @Date:   2018-06-05 21:17:12
+ * @Last Modified by:   Administrator
+ * @Last Modified time: 2018-06-05 21:18:19
+ */
+'use strict';
+
+require('page/common/header/index.js');
+require('page/common/nav/index.js');
