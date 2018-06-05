b9263508978edb47ea77eeea9f3afec1bf12c55a
AlbertRui <isruizhang@gmail.com>
2018-06-05 21:15:02 +0800

    update config file for project

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,22 @@
  * @Author: Administrator
  * @Date:   2018-05-21 17:11:42
  * @Last Modified by:   Administrator
- * @Last Modified time: 2018-05-22 22:57:03
+ * @Last Modified time: 2018-06-05 21:14:25
  */
 const path = require('path');
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// 环境变量配置，dev / online
+//环境变量配置，dev/online
 var WEBPACK_ENV = process.env.WEBPACK_ENV || 'dev';
 
-// 获取html-webpack-plugin参数的方法 
+//获取html-webpack-plugin参数的方法。
 var getHtmlConfig = function(name, title) {
     return {
         template: './src/view/' + name + '.html',
         filename: 'view/' + name + '.html',
+        favicon: './favicon.ico',
         title: title,
         inject: true,
         hash: true,
@@ -25,24 +26,28 @@ var getHtmlConfig = function(name, title) {
 };
 // webpack config
 var config = {
-    mode: 'production',
     entry: {
         'common': ['./src/page/common/index.js'],
         'index': ['./src/page/index/index.js'],
         'list': ['./src/page/list/index.js'],
         'detail': ['./src/page/detail/index.js'],
         'cart': ['./src/page/cart/index.js'],
+        'order-confirm': ['./src/page/order-confirm/index.js'],
+        'order-list': ['./src/page/order-list/index.js'],
+        'order-detail': ['./src/page/order-detail/index.js'],
+        'payment': ['./src/page/payment/index.js'],
         'user-login': ['./src/page/user-login/index.js'],
         'user-register': ['./src/page/user-register/index.js'],
-        'user-pass-reset': ['./src/page/user-pass-reset/index.js'],
         'user-center': ['./src/page/user-center/index.js'],
         'user-center-update': ['./src/page/user-center-update/index.js'],
         'user-pass-update': ['./src/page/user-pass-update/index.js'],
+        'user-pass-reset': ['./src/page/user-pass-reset/index.js'],
         'result': ['./src/page/result/index.js'],
+        'about': ['./src/page/about/index.js']
     },
     output: {
-        path: path.resolve(__dirname, './dist'),
-        publicPath: '/dist',
+        path: __dirname + '/dist/',
+        publicPath: 'dev' === WEBPACK_ENV ? '/dist/' : '//s.imitationtmall.xin/mmall-fe/dist/',
         filename: 'js/[name].js'
     },
     externals: {
@@ -57,7 +62,11 @@ var config = {
             loader: 'url-loader?limit=100&name=resource/[name].[ext]'
         }, {
             test: /\.string$/,
-            loader: 'html-loader'
+            loader: 'html-loader',
+            query: {
+                minimize: true,
+                removeAttributeQuotes: false
+            }
         }]
     },
     resolve: {
@@ -70,30 +79,34 @@ var config = {
         }
     },
     plugins: [
-        // 独立通用模块到js/base.js
+        //独立通用模块
         new webpack.optimize.CommonsChunkPlugin({
             name: 'common',
             filename: 'js/base.js'
         }),
-        // 把css单独打包到文件里
+        //将css单独打包到文件
         new ExtractTextPlugin("css/[name].css"),
-        // html模板的处理
+        //html模板处理
+        new HtmlWebpackPlugin(getHtmlConfig('user-register', '用户注册')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-login', '用户登陆')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-center', '个人中心')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-center-update', '修改个人信息')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-pass-update', '修改密码')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-pass-reset', '找回密码')),
+        new HtmlWebpackPlugin(getHtmlConfig('order-confirm', '确认订单')),
+        new HtmlWebpackPlugin(getHtmlConfig('order-list', '订单列表')),
+        new HtmlWebpackPlugin(getHtmlConfig('order-detail', '订单详情')),
+        new HtmlWebpackPlugin(getHtmlConfig('payment', '订单支付')),
         new HtmlWebpackPlugin(getHtmlConfig('index', '首页')),
         new HtmlWebpackPlugin(getHtmlConfig('list', '商品列表页')),
         new HtmlWebpackPlugin(getHtmlConfig('detail', '商品详情页')),
         new HtmlWebpackPlugin(getHtmlConfig('cart', '购物车')),
-        new HtmlWebpackPlugin(getHtmlConfig('user-login', '用户登录')),
-        new HtmlWebpackPlugin(getHtmlConfig('user-register', '用户注册')),
-        new HtmlWebpackPlugin(getHtmlConfig('user-pass-reset', '找回密码')),
-        new HtmlWebpackPlugin(getHtmlConfig('user-center', '个人中心')),
-        new HtmlWebpackPlugin(getHtmlConfig('user-center-update', '修改个人信息')),
-        new HtmlWebpackPlugin(getHtmlConfig('user-pass-update', '修改密码')),
         new HtmlWebpackPlugin(getHtmlConfig('result', '操作结果')),
+        new HtmlWebpackPlugin(getHtmlConfig('about', '关于页面'))
     ]
-};
 
+};
 if ('dev' === WEBPACK_ENV) {
     config.entry.common.push('webpack-dev-server/client?http://localhost:8088/');
 }
-
 module.exports = config;
