0fec7416332f051f27783d209ffe8ace0db41241
AlbertRui <isruizhang@gmail.com>
2018-07-13 22:49:05 +0800

    modified

--- src/page/user-login/index.js
+++ src/page/user-login/index.js
@@ -2,21 +2,19 @@
  * @Author: Administrator
  * @Date:   2018-05-21 17:42:58
  * @Last Modified by:   Administrator
- * @Last Modified time: 2018-05-26 17:19:12
+ * @Last Modified time: 2018-07-13 22:46:49
  */
-'user strict';
 require('./index.css');
 require('page/common/nav-simple/index.js');
-var _ubaby = require('util/ubaby.js');
 var _user = require('service/user-service.js');
-
-//表单里的错误提示
+var _ubaby = require('util/ubaby.js');
+// 表单里的错误提示
 var formError = {
-    show: function(errorMsg) {
-        $('.error-item').show().find('error-msg').text(errorMsg);
+    show: function(errMsg) {
+        $('.error-item').show().find('.err-msg').text(errMsg);
     },
     hide: function() {
-        $('.error-item').hide().find('error-msg').text('');
+        $('.error-item').hide().find('.err-msg').text('');
     }
 };
 
@@ -27,19 +25,16 @@ var page = {
     },
     bindEvent: function() {
         var _this = this;
-        //登录按钮的点击
         $('#submit').click(function() {
             _this.submit();
         });
-        //如果按下回车，也进行提交
-        $('user-content').keyup(function(e) {
-            //keyCode为13表示回车键
+        $('.user-content').keyup(function(e) {
+            //回车键监听
             if (e.keyCode === 13) {
                 _this.submit();
             }
         });
     },
-    //提交表单
     submit: function() {
         var formData = {
                 username: $.trim($('#username').val()),
@@ -47,36 +42,37 @@ var page = {
             },
             //表单验证结果
             validateResult = this.formValidate(formData);
-        //验证成功
         if (validateResult.status) {
+            // 验证成功
             _user.login(formData, function(res) {
                 window.location.href = _ubaby.getUrlParam('redirect') || './index.html';
-            }, function(errorMsg) {
-                formError.show(errorMsg);
+            }, function(err) {
+                formError.show(err);
             });
-        } else { //验证失败，错误提示
+        } else {
+            // 验证失败
             formError.show(validateResult.msg);
         }
     },
-    //表单字段验证
     formValidate: function(formData) {
         var result = {
             status: false,
             msg: ''
         };
-        if (!_ubaby.validate(fromData.username, 'require')) {
+        if (!_ubaby.validate(formData.username, 'require')) {
             result.msg = '用户名不能为空';
             return result;
         }
-        if (!_ubaby.validate(fromData.password, 'require')) {
+        if (!_ubaby.validate(formData.password, 'require')) {
             result.msg = '密码不能为空';
             return result;
         }
         result.status = true;
-        result.msg = '验证通过';
+        result.msg = "验证通过";
         return result;
     }
 };
+//当JQuery加载完成的时候会触发
 $(function() {
     page.init();
 });
