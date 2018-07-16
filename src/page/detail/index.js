d92ba7cc1fbe7a7f3ad82fac002083508ff92180
AlbertRui <isruizhang@gmail.com>
2018-07-16 22:20:51 +0800

    update

--- src/page/detail/index.js
+++ src/page/detail/index.js
@@ -2,54 +2,64 @@
  * @Author: Administrator
  * @Date:   2018-05-27 20:01:08
  * @Last Modified by:   Administrator
- * @Last Modified time: 2018-05-27 22:27:31
+ * @Last Modified time: 2018-07-16 22:11:23
  */
 'use strict';
 
 require('./index.css');
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
-var _ubaby = require('util/ubaby.js');
 var _product = require('service/product-service.js');
 var _cart = require('service/cart-service.js');
-var templateIndex = require('./index.string');
+var _ubaby = require('util/ubaby.js');
+var templateHtml = require('./index.string');
 
 var page = {
     data: {
-        productId: _ubaby.getUrlParam('productId') || '',
+        productId: _ubaby.getUrlParam('productId') || ''
     },
     init: function() {
         this.onLoad();
         this.bindEvent();
     },
     onLoad: function() {
-        // 如果没有传productId, 自动跳回首页
         if (!this.data.productId) {
             _ubaby.goHome();
+            return;
         }
         this.loadDetail();
     },
     bindEvent: function() {
         var _this = this;
         // 图片预览
+        // 当前版本jQuery不能使用hover，使用mouseenter（鼠标进入）来代替
         $(document).on('mouseenter', '.p-img-item', function() {
-            var imageUrl = $(this).find('.p-img').attr('src');
-            $('.main-img').attr('src', imageUrl);
+            $(this).addClass('select')
+                .siblings('.p-img-item').removeClass('select');
+            var imgUrl = $(this).find('.p-img').attr('src');
+            $('.main-img').attr('src', imgUrl);
         });
-        // count的操作
+        //购买数量点击
         $(document).on('click', '.p-count-btn', function() {
             var type = $(this).hasClass('plus') ? 'plus' : 'minus',
                 $pCount = $('.p-count'),
-                currCount = parseInt($pCount.val()),
+                currentCount = parseInt($pCount.val()),
                 minCount = 1,
                 maxCount = _this.data.detailInfo.stock || 1;
             if (type === 'plus') {
-                $pCount.val(currCount < maxCount ? currCount + 1 : maxCount);
+                if (currentCount >= maxCount) {
+                    return;
+                }
+                $pCount.val(currentCount + 1);
             } else if (type === 'minus') {
-                $pCount.val(currCount > minCount ? currCount - 1 : minCount);
+                if (currentCount <= minCount) {
+                    return;
+                }
+                $pCount.val(currentCount - 1);
             }
+            _this.refreshCountBtn($pCount.val(), maxCount);
         });
-        // 加入购物车
+        //添加购物车点击
         $(document).on('click', '.cart-add', function() {
             _cart.addToCart({
                 productId: _this.data.productId,
@@ -61,28 +71,38 @@ var page = {
             });
         });
     },
-    // 加载商品详情的数据
+    //加载商品详细信息
     loadDetail: function() {
         var _this = this,
             html = '',
             $pageWrap = $('.page-wrap');
-        // loading
-        $pageWrap.html('<div class="loading"></div>');
-        // 请求detail信息
+        _ubaby.showLoading('.page-wrap');
+        //请求商品详细信息
         _product.getProductDetail(this.data.productId, function(res) {
-            _this.filter(res);
-            // 缓存住detail的数据
+            // 缓存数据
             _this.data.detailInfo = res;
-            // render
-            html = _ubaby.renderHtml(templateIndex, res);
+            _this.filter(res);
+            html = _ubaby.renderHtml(templateHtml, res);
             $pageWrap.html(html);
+            _this.refreshCountBtn($('.p-count').val(), res.stock);
         }, function(errMsg) {
-            $pageWrap.html('<p class="err-tip">此商品太淘气，找不到了</p>');
-        });
+            _ubaby.showErrorMessage($pageWrap, '此商品太淘气找不到了');
+        })
     },
     // 数据匹配
     filter: function(data) {
+        // 因为是引用传递，所以不需要返回
         data.subImages = data.subImages.split(',');
+    },
+    refreshCountBtn: function(currentCount, maxCount) {
+        if (parseInt(currentCount) === 1) {
+            $('.minus').addClass('full');
+        } else if (parseInt(currentCount) === maxCount) {
+            $('.plus').addClass('full');
+        } else {
+            $('.plus').removeClass('full');
+            $('.minus').removeClass('full');
+        }
     }
 };
 $(function() {
